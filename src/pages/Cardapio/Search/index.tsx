28292665b7d0ae14e20ac2fc44f566cacec712a5
harpia01 <andre23arruda@gmail.com>
2022-04-24 12:24:35 -0400

    search props

---
+++ src/pages/Cardapio/Search/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+interface SearchProps {
+	search: string,
+	setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Search({ search, setSearch }: SearchProps) {
+	return (
+		<div>
+		</div>
+	)
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
+
+import Search from './Search'
+
 import styles from './Cardapio.module.scss'
 // import logo from 'assets/images/logo.svg'
 import { ReactComponent as Logo} from 'assets/images/logo.svg'
 
 export default function Cardapio() {
+	const [search, setSearch] = useState('')
+
 	return (
 		<main>
 			<nav className={ styles.menu }>
@@ -15,6 +21,14 @@ export default function Cardapio() {
 					A casa do código e da massa
 				</div>
 			</header>
+
+			<section>
+				<h3>Cardárpio</h3>
+				<Search
+					search={ search }
+					setSearch={ setSearch }
+				/>
+			</section>
 		</main>
 	)
 }
