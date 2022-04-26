261feb2481f447fda7be5f7486352b7b30fcecd9
harpia01 <andre23arruda@gmail.com>
2022-04-26 14:45:25 -0400

    Add links at menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,26 @@
+import { Link } from 'react-router-dom'
 import { ReactComponent as Logo} from 'assets/images/logo.svg'
 import styles from './Menu.module.scss'
+
+const routes = [
+	{ label: 'Início', to: '/'},
+	{ label: 'Cardárpio', to: '/cardapio'},
+	{ label: 'Sobre', to: '/sobre'},
+]
 
 export default function Menu() {
 	return (
 		<nav className={ styles.menu }>
 			<Logo />
+
+			<ul>
+				{ routes.map(route => (
+					<li key={ route.label }>
+						<Link to={ route.to }>
+							{ route.label }
+						</Link>
+					</li>
+				))}
+			</ul>
 		</nav>
 	)}
