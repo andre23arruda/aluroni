093de4808c53403f58afc5476573efb1606a070f
harpia01 <andre23arruda@gmail.com>
2022-04-27 11:39:27 -0400

    useParams

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -3,12 +3,15 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import classNames from 'classnames'
 
 import items from 'data/cardapio.json'
+import { ReactComponent as NotFoundImage } from 'assets/images/not_found.svg'
+
 
 export default function Prato() {
-	console.log(useParams())
-
+	const { id } = useParams()
 	const { state } = useLocation()
-	const { item } = state as { item: typeof items[0] }
+	// const { item } = state as { item: typeof items[0] }
+	const item = items.find(item => item.id === Number(id))
+	console.log(state)
 
 	const navigate = useNavigate()
 
@@ -22,40 +25,46 @@ export default function Prato() {
 				{ '< Voltar' }
 			</button>
 
-			<h1 className={ styles.title }>
-				{ item.title }
-			</h1>
+			{ item ? (
+				<>
+					<h1 className={ styles.title }>
+						{ item.title }
+					</h1>
 
-			<div className={ styles.imageContainer }>
-				<img src={ item.photo } alt={ item.title } />
-			</div>
-
-			<div className={ styles.content }>
-				<p className={ styles.description }>
-					{ item.description }
-				</p>
-
-				<div className={ styles.tags }>
-					<div className={classNames({
-						[styles.type]: true,
-						[styles[item.category.label.toLowerCase()]]: true,
-					})}>
-						{ item.category.label }
+					<div className={ styles.imageContainer }>
+						<img src={ item.photo } alt={ item.title } />
 					</div>
 
-					<div className={ styles.portion }>
-						{ item.size }g
-					</div>
+					<div className={ styles.content }>
+						<p className={ styles.description }>
+							{ item.description }
+						</p>
 
-					<div className={ styles.people }>
-						Serve { item.serving } pessoa{ item.serving > 1 ? 's' : '' }
-					</div>
+						<div className={ styles.tags }>
+							<div className={classNames({
+								[styles.type]: true,
+								[styles[item.category.label.toLowerCase()]]: true,
+							})}>
+								{ item.category.label }
+							</div>
 
-					<div className={ styles.price }>
-						R${ item.price.toFixed(2) }
+							<div className={ styles.portion }>
+								{ item.size }g
+							</div>
+
+							<div className={ styles.people }>
+								Serve { item.serving } pessoa{ item.serving > 1 ? 's' : '' }
+							</div>
+
+							<div className={ styles.price }>
+								R${ item.price.toFixed(2) }
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				</>
+			) : (
+				<NotFoundImage />
+			)}
 		</section>
 
 	)
